af36381c91a6ccbb39b4952894e0172505f32bd2
Yong Jin, Cho <drajin.cho@gmail.com>
2021-01-29 15:04:40 +0900

    User.turn 초기화 추가

--- Python/user.py
+++ Python/user.py
@@ -3,6 +3,7 @@ from player import *
 class User(Player):
     def __init__(self, othello):
         super().__init__(othello)
+        self.turn = False
 
     def onTurn(self):
         self.turn = True
